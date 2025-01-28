8d97068ae94144bd37ec5278daaf5ddbea2c41e6
hthomas <hiks13@gmail.com>
2025-01-28 16:28:24 +0000

    Changed function name and added data types

--- Day-6/part-2.py
+++ Day-6/part-2.py
@@ -1,7 +1,9 @@
 import timeit
 from TestHelper import generate_file_name
 
-def findPath(currRow, currCol, matrix, visited = None, direction = 0, checkLoop = False):
+
+def findLoopCount(currRow: int, currCol: int, matrix: [[int]],
+                  visited: set = None, direction: int = 0, checkLoop: bool = False):
     if visited is None:
         visited = set()
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
@@ -12,7 +14,8 @@ def findPath(currRow, currCol, matrix, visited = None, direction = 0, checkLoop 
     while 0 <= currRow < len(matrix) and 0 <= currCol < len(matrix[0]):
 
         if not checkLoop:
-            if 1 <= currRow < len(matrix) - 1 and 1 <= currCol < len(matrix[0]) - 1 and matrix[currRow + nextRow][currCol + nextCol] != "#":
+            if 1 <= currRow < len(matrix) - 1 and 1 <= currCol < len(matrix[0]) - 1 and matrix[currRow + nextRow][
+                currCol + nextCol] != "#":
                 matrix[currRow + nextRow][currCol + nextCol] = "#"
 
                 alreadyVisited = False
@@ -20,7 +23,7 @@ def findPath(currRow, currCol, matrix, visited = None, direction = 0, checkLoop 
                     if (currRow + nextRow, currCol + nextCol, currDir) in visited:
                         alreadyVisited = True
 
-                if not alreadyVisited and findPath(currRow, currCol, matrix, set(), direction, True):
+                if not alreadyVisited and findLoopCount(currRow, currCol, matrix, set(), direction, True):
                     count += 1
                 matrix[currRow + nextRow][currCol + nextCol] = "."
         else:
@@ -30,7 +33,8 @@ def findPath(currRow, currCol, matrix, visited = None, direction = 0, checkLoop 
         visited.add((currRow, currCol, direction))
 
         # If next position is blocked, change direction and go to the start of the loop without moving
-        if 0 <= currRow + nextRow < len(matrix) and 0 <= currCol + nextCol < len(matrix[0]) and matrix[currRow + nextRow][currCol + nextCol] == '#':
+        if 0 <= currRow + nextRow < len(matrix) and 0 <= currCol + nextCol < len(matrix[0]) and \
+                matrix[currRow + nextRow][currCol + nextCol] == '#':
             direction = (direction + 1) % 4
             nextRow = directions[direction][0]
             nextCol = directions[direction][1]
@@ -45,17 +49,17 @@ def findPath(currRow, currCol, matrix, visited = None, direction = 0, checkLoop 
     # Fix to use timeit.timeit()
     return str(count)
 
+
 def main():
     startRow, startCol, matrix = importTests()
-    print(findPath(startRow, startCol, matrix))
+    print(findLoopCount(startRow, startCol, matrix))
 
     # Execute 1000 times to estimate time
-    executionTime = timeit.timeit(findPath(startRow, startCol, matrix), number=1000)
+    executionTime = timeit.timeit(findLoopCount(startRow, startCol, matrix), number=1000)
     print(f"Execution time: {executionTime:.6f} seconds")
 
 
 def importTests():
-
     # Build the map
     matrix = []
     startRow, startCol = 0, 0
@@ -64,6 +68,8 @@ def importTests():
         lines = f.readlines()
         for i in range(len(lines)):
             line = [i for i in lines[i].strip()]
+
+            # Find starting index on the fly!
             if '^' in line:
                 startRow = i
                 startCol = line.index('^')
---
+++ Day-7/part-1.py
@@ -0,0 +1,10 @@
+from TestHelper import generate_file_name
+
+def findCalibrationResult():
+    return 0
+
+def main():
+    print(findCalibrationResult())
+
+if __name__ == "__main__":
+    main()
